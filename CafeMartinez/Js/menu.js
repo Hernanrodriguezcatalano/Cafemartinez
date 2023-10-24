27ec8013ebae82646b51fc44ca0e4e70f2f2c3cf
Hernanrodriguezcatalano <hernanrodriguezcatalano@gmail.com>
2023-10-24 01:32:46 -0300

    Add files via upload

---
+++ CafeMartinez/Js/menu.js
@@ -0,0 +1,129 @@
+//creo una variable ALT + 96
+let miMenu=`
+
+
+
+</div>
+<div class="contenedor">
+    <main class="contenido">
+        <div class="fila">
+            <div class="contenido-favorito">
+                <h5>Opciones</h5>
+            </div>
+            <div class="rojo">
+                <a href="../Tradicional/Cafeteria.html">
+                    <h5>Cafeteria</h5>
+                </a>
+            </div>
+            <div class="azul">
+                <a href="../Tradicional/Sugerencias.html">
+                    <h5>Sugerencias</h5>
+                </a>
+            </div>
+            <div class="amarillo">
+                <a href="../Tradicional/MediaTostFac.html">
+                    <h6>Medialunas tostadas Facturas</h6>
+                </a>
+            </div>
+            <div class="rosado">
+                <a href="../Tradicional/AdiDif.html">
+                    <h6>Adicionales & Diferencias</h6>
+                </a>
+            </div>
+            <div class="turquesa">
+                <a href="../Tradicional/AlCena.html">
+                    <h6>Almuerzo Cenas</h6>
+                </a>
+            </div>
+            <div class="negro">
+                <a href="../Tradicional/Sandwiches.html">
+                    <h5>Sandwiches</h5>
+                </a>
+            </div>
+            <div class="rojo ">
+                <a href="../Tradicional/Gaseosas.html">
+                    <h6>Gaseosas & Aguas Saborizadas</h6>
+                </a>
+            </div>
+            <div class="rosado">
+                <a href="../Tradicional/StaccKosher.html">
+                    <h6>Stacc Kosher</h6>
+                </a>
+            </div>
+            <div class="cyanclaro">
+                <a href="../Tradicional/MuebleEx.html">
+                    <h6>Mueble Express</h6>
+                </a>
+            </div>
+            <div class="blanco">
+                <a href="../Tradicional/Mercha.html">
+                    <h5>Merchandising</h5>
+                </a>
+            </div>
+            <div class="blanco">
+                <a href="../Tradicional/Molienda.html">
+                    <h5>Molienda</h5>
+                </a>
+            </div>
+            <div class="verde">
+                <a href="../Tradicional/Torta.html">
+                    <h5>Tortas</h5>
+                </a>
+            </div>
+            <div class="verdeclaro">
+                <a href="../Tradicional/Delicia.html">
+                    <h5>Delicias</h5>
+                </a>
+            </div>
+            <div class="negro">
+                <a href="../Tradicional/JugosLiGra.html">
+                    <h6>Jugos Licuados Granizados</h6>
+                </a>
+            </div>
+            <div class="negro">
+                <a href="../Tradicional/Almacen.html">
+                    <h5>Almacen</h5>
+                </a>
+            </div>
+            <div class="marron">
+                <a href="../Tradicional/BebAlc.html">
+                    <h6>Bebidas con Alcohol</h6>
+                </a>
+            </div>
+        </div>
+        <br>
+        <hr>
+        <br>
+
+`
+document.querySelector("section").innerHTML = miMenu
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
